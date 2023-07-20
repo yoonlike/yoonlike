644e1e43418559f8d2ed3d8ea8997719a4de3d51
yoonlike <140046884+yoonlike@users.noreply.github.com>
2023-07-20 15:21:23 +0900

    요리 타이머

--- main.ts
+++ main.ts
@@ -1,5 +1,21 @@
-let 시간 = 0
-basic.showNumber(시간)
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    시간 += 1
+    basic.showNumber(시간)
 })
+input.onGesture(Gesture.Shake, function () {
+    시간 = 0
+    basic.showNumber(시간)
+})
+input.onButtonPressed(Button.B, function () {
+    while (시간 > 0) {
+        basic.pause(60000)
+        시간 += -1
+    }
+    for (let index = 0; index < 2; index++) {
+        music.play(music.stringPlayable("C D E F G A B C5 ", 120), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
+    }
+})
+let 시간 = 0
+시간 = 0
+basic.showNumber(시간)
